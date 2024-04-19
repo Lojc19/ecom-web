60743779af6eb4ccb1384eb00f74006168328574
LAPTOP-182PLMJF\PC <20110615@student.hcmute.edu.vn>
2024-04-19 23:26:16 +0700

    add auth form

---
+++ src/components/Form/AuthForm.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const AuthForm = () => {
+  return (
+    <>
+      <div className="flex flex-row p-4">
+        <div class="basis-1/2">
+            <h2 className="text-3xl font-semibold">ĐĂNG NHẬP</h2>
+            <h3 className="text-xl font-semibold my-2">Tên đăng nhập hoặc địa chỉ email *</h3>
+            <div className="flex flex-row w-full">
+                <input
+                    type="text"
+                    className="form-control  w-full mr-16 p-4 border-2 border-gray-400"
+                    placeholder=""
+                />
+            </div>
+            <h3 className="text-xl font-semibold my-2 mt-8">Mật khẩu *</h3>
+            <div className="flex flex-row w-full">
+                <input
+                    type="text"
+                    className="form-control  w-full mr-16 p-4 border-2 border-gray-400"
+                    placeholder=""
+                />
+            </div>
+            <div className="flex flex-row w-full mt-4">
+                <input
+                    type="checkbox"
+                    className="form-control p-4 border-2 border-gray-400"
+                    placeholder=""
+                />
+                <h3 className="text-xl font-semibold mx-4">Lưu thông tin đăng nhập</h3>
+            </div>
+            
+            <div className="flex flex-row w-full mt-2">
+                <button className="bg-black border border-black text-[20px] px-6 py-2 uppercase text-white cursor-pointer mt-2" >
+                    ĐĂNG NHẬP
+                </button>
+            </div>
+            <div className="flex flex-row w-full mt-2">
+                <a className="text-xl font-semibold">Quên mật khẩu?</a>
+            </div>
+        </div>
+        <div class="basis-1/2">
+            <h2 className="text-3xl font-semibold">ĐĂNG KÝ</h2>
+            <h3 className="text-xl font-semibold my-2">Địa chỉ email *</h3>
+            <div className="flex flex-row w-full">
+                <input
+                    type="text"
+                    className="form-control  w-full mr-16 p-4 border-2 border-gray-400"
+                    placeholder=""
+                />
+            </div>
+            <h3 className="text-xl font-normal my-4">Mật khẩu mới sẽ được gửi tới email của bạn.*</h3>
+            <h3 className="text-lg font-normal my-4">
+                Dữ liệu cá nhân của bạn sẽ được sử dụng để hỗ trợ trải nghiệm của bạn trên toàn bộ trang web này, để quản lý quyền truy cập vào tài khoản của bạn và cho các mục đích khác được mô tả trong chính sách bảo mật của chúng tôi.
+            </h3>
+            <div className="flex flex-row w-full mt-2">
+                <button className="bg-black border border-black text-[20px] px-6 py-2 uppercase text-white cursor-pointer mt-2" >
+                    ĐĂNG KÝ
+                </button>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthForm;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BiSolidPhoneCall } from "react-icons/bi";
 import { IoCartOutline, IoHeartOutline, IoLocationOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
-
+import { Modal } from "antd";
+import AuthForm from "../Form/AuthForm";
 const Header = () => {
+    const [visible, setVisible] = useState(false);
     return (
         <div className="hidden md:block h-[55px] w-full border-b-[1px] p-3">
             <div className="mx-auto md:max-w-[1320px] container h-full flex justify-between items-center text-[14px] font-Roboto">
@@ -26,9 +28,17 @@ const Header = () => {
                     <a href=""><IoLocationOutline size={22}/></a>
                     <a href=""><IoHeartOutline size={22}/></a>
                     <a href=""><IoCartOutline size={22}/></a>
-                    <a href="" className=" text-[#666666D9] hover:text-black">Đăng nhập <FaRegUser className="inline-block ml-1" size={20}/></a>
+                    <button href="" className=" text-[#666666D9] hover:text-black" onClick={()=>{setVisible(true)}}>Đăng nhập <FaRegUser className="inline-block ml-1" size={20}/></button>
                 </div>
             </div>
+            <Modal
+                onCancel={() => setVisible(false)}
+                width={1000} closable={false} footer={null}
+                visible={visible}>
+                <AuthForm
+                    
+                />
+            </Modal>
         </div>
     );
 };
